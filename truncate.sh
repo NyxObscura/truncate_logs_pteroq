e04386743f5e481b9d84a4e9f6c486a9ac5a9e9d
NyxObscura <service@obscuraworks.com>
2025-07-13 08:56:42 +0700

    Update truncate.sh

--- truncate.sh
+++ truncate.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-mysql -u root -e "TRUNCATE panel.activity_log_subjects; TRUNCATE panel.activity_logs;"
+mysql -u root <<EOF
+SET FOREIGN_KEY_CHECKS=0;
+DELETE FROM panel.activity_log_subjects;
+DELETE FROM panel.activity_logs;
+SET FOREIGN_KEY_CHECKS=1;
+EOF
